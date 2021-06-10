daf5f36ef01f95e3844abfa3fe08b1a984d703c5
Mahmoud Kassah <kassah.webdev@gmail.com>
2021-06-10 16:04:11 +0300

    add store

--- packages/front/src/components/StoresTable.jsx
+++ packages/front/src/components/StoresTable.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { DataGrid } from "@material-ui/data-grid";
 import { fetchAllStores } from "../features/stores/storesAction";
+import Modal from "./shared/Modal";
+import { Button } from "@material-ui/core";
+import AddStore from "./AddStore";
 
 const StoresTable = () => {
   const { stores, isLoading, error } = useSelector((state) => {
@@ -12,6 +15,10 @@ const StoresTable = () => {
     size: stores.size || 5,
     currentPage: stores.currentPage || 0,
   });
+
+  const [open, setOpen] = useState(false);
+  const handleOpenModal = () => setOpen(true);
+  const handleCloseModal = () => setOpen(false);
 
   const dispatch = useDispatch();
 
@@ -38,41 +45,69 @@ const StoresTable = () => {
   ];
 
   return (
-    <div className="flex h-full">
-      {/* {isLoading && <div>Loading...</div>} */}
-      {error ? (
-        <div className="text-red-500">{error}</div>
-      ) : stores.data && stores.data.length > 0 ? (
-        <div className="w-full h-[400px]">
-          <DataGrid
-            rows={stores.data}
-            columns={columns}
-            page={pagination.currentPage}
-            pageSize={pagination.size}
-            rowsPerPageOptions={[5, 10, 15]}
-            paginationMode="server"
-            loading={isLoading}
-            rowCount={stores.count}
-            onPageChange={(params) => {
-              setPagination((pagination) => ({
-                ...pagination,
-                currentPage: params.page,
-              }));
-            }}
-            onPageSizeChange={(params) => {
-              setPagination((pagination) => ({
-                ...pagination,
-                size: params.pageSize,
-              }));
-            }}
-          />
-        </div>
-      ) : (
-        <div className="my-4 text-lg font-medium text-gray-300">
-          It seems there are no stores yes
-        </div>
-      )}
-    </div>
+    <>
+      <Modal
+        title="Create New Store"
+        open={open}
+        handleClose={handleCloseModal}
+      >
+        <AddStore
+          onSuccess={fetchAllStores.bind(null, {
+            size: pagination.size,
+            page: pagination.currentPage + 1,
+          })}
+        />
+      </Modal>
+      <div className="flex h-[600px]">
+        {/* {isLoading && <div>Loading...</div>} */}
+        {error ? (
+          <div className="text-red-500">{error}</div>
+        ) : stores.data && stores.data.length > 0 ? (
+          <div className="w-full h-full">
+            <div className="w-full flex py-2 justify-start">
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={handleOpenModal}
+              >
+                Add New Store
+              </Button>
+            </div>
+            <div className="w-full h-[70%]">
+              <DataGrid
+                rows={stores.data}
+                columns={columns}
+                page={pagination.currentPage}
+                pageSize={pagination.size}
+                rowsPerPageOptions={[5, 10, 15]}
+                paginationMode="server"
+                loading={isLoading}
+                rowCount={stores.count}
+                onPageChange={(params) => {
+                  setPagination((pagination) => ({
+                    ...pagination,
+                    currentPage: params.page,
+                  }));
+                }}
+                onPageSizeChange={(params) => {
+                  setPagination((pagination) => ({
+                    ...pagination,
+                    size: params.pageSize,
+                  }));
+                }}
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="my-4 text-lg font-medium text-gray-300">
+            It seems there are no stores yes
+          </div>
+        )}
+      </div>
+      <Modal>
+        <div>test modal</div>
+      </Modal>
+    </>
   );
 };
 
